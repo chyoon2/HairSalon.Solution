d04cc0a908dac13fec26da6a764bb6bd4e5f41c2
Chris Yoon <chy.yoon@gmail.com>
2020-07-31 09:57:17 -0700

    add index.cshtml functionality

--- Salon/Controllers/ClientsController.cs
+++ Salon/Controllers/ClientsController.cs
@@ -11,10 +11,12 @@ namespace Salon.Controllers
   public class ClientsController : Controller
   {
     private readonly SalonContext _db;
+    
     public ClientsController(SalonContext db)
     {
       _db = db;
     }
+
     public ActionResult Index()
     {
       List<Client> model = _db.Clients.Include(Clients => Clients.Stylist).ToList();
