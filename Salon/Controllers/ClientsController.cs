6b5ba53e0444c0de9630bb8e4e67b0f36f54b650
Chris Yoon <chy.yoon@gmail.com>
2020-07-31 11:22:24 -0700

    add routes to delete and edit and delete view

--- Salon/Controllers/ClientsController.cs
+++ Salon/Controllers/ClientsController.cs
@@ -42,6 +42,33 @@ namespace Salon.Controllers
       _db.SaveChanges();
       return RedirectToAction("Index");
     }
-    
+
+    public ActionResult Edit(int id)
+    {
+      Client thisClient = _db.Clients.FirstOrDefault(Clients => Clients.ClientId == id);
+      ViewBag.StylistId = new SelectList(_db.Stylists, "StylistId", "Name");
+      return View(thisClient);
+    }
+    [HttpPost]
+    public ActionResult Edit(Client client)
+    {
+      _db.Entry(client).State = EntityState.Modified;
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Delete(int id)
+    {
+      Client thisClient = _db.Clients.FirstOrDefault(Clients => Clients.ClientId == id);
+      return View(thisClient);
+    }
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+      Client thisClient = _db.Clients.FirstOrDefault(Clients => Clients.ClientId == id);
+      _db.Clients.Remove(thisClient);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
   }
 }
